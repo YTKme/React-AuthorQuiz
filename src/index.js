3c8e963859e9e501b52a82bedb69117e591aab8d
Yan Kuang <YTKme@Outlook.com>
2019-02-05 15:01:39 -0800

    Completed Author Quiz project

--- src/index.js
+++ src/index.js
@@ -1,10 +1,142 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, withRouter } from 'react-router-dom';
+import * as Redux from 'redux';
+import * as ReactRedux from 'react-redux';
 import './index.css';
-import App from './App';
+import AuthorQuiz from './AuthorQuiz';
+import AddAuthorForm from './AddAuthorForm';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const authors = [
+    {
+        name: "Mark Twain",
+        imageUrl: "images/author/marktwain.jpg",
+        imageSource: "Wikimedia Commons",
+        books: [
+            'The Adventures of Huckleberry Finn',
+            'Life on the Mississippi',
+            'Roughing It',
+        ],
+    },
+    {
+        name: "Joseph Conrad",
+        imageUrl: "images/author/josephconrad.png",
+        imageSource: "Wikimedia Commons",
+        books: [
+            'Heart of Darkness',
+        ],
+    },
+    {
+        name: "J.K. Rowling",
+        imageUrl: "images/author/jkrowling.jpg",
+        imageSource: "Wikimedia Commons",
+        imageAttribution: "Daniel Ogren",
+        books: [
+            'Harry Potter and the Sorcerers Stone',
+        ],
+    },
+    {
+        name: "Stephen King",
+        imageUrl: "images/author/stephenking.png",
+        imageSource: "Wikimedia Commons",
+        imageAttribution: "Pinguino",
+        books: [
+            'The Shining',
+            'IT',
+        ],
+    },
+    {
+        name: "Charles Dickens",
+        imageUrl: "images/author/charlesdickens.jpg",
+        imageSource: "Wikimedia Commons",
+        books: [
+            'David Copperfield',
+            'A Tale of Two Cities',
+        ],
+    },
+    {
+        name: "William Shakespeare",
+        imageUrl: "images/author/williamshakespeare.jpg",
+        imageSource: "Wikimedia Commons",
+        books: [
+            'Hamlet',
+            'Macbeth',
+            'Romeo and Juliet'
+        ],
+    },
+];
+
+function getTurnData(authors) {
+    // Reduce author's collection and concatenating them into a larger set
+    const allBooks = authors.reduce(function (p, c, i) {
+        return p.concat(c.books);
+    }, []);
+
+    // Shuffle list into random order
+    const fourRandomBooks = shuffleArray(allBooks).slice(0, 4);
+
+    const answer = fourRandomBooks[0];
+
+    return ({
+        books: fourRandomBooks,
+        author: authors.find(
+            (author) => author.books.some(
+                (title) => title === answer
+            )
+        ),
+    });
+}
+
+// Shuffle array in place
+function shuffleArray(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+
+    return a;
+}
+
+function reducer(
+    state = {
+        authors,
+        turnData: getTurnData(authors),
+        highlight: ''
+    }, action) {
+        switch (action.type) {
+            case 'ANSWER_SELECTED':
+                const isCorrect = state.turnData.author.books.some((book) => book === action.answer);
+                return (
+                    Object.assign({}, state, { highlight: isCorrect ? 'correct' : 'wrong' })
+                );
+            case 'CONTINUE':
+                return (
+                    Object.assign({}, state, { highlight: '', turnData: getTurnData(state.authors) })
+                );
+            case 'ADD_AUTHOR':
+                return (
+                    Object.assign({}, state, { authors: state.authors.concat([action.author]) })
+                );
+            default:
+                return state;
+        }
+}
+
+let store = Redux.createStore(
+    reducer
+);
+
+ReactDOM.render(
+    <BrowserRouter>
+        <ReactRedux.Provider store={store}>
+            <React.Fragment>
+                <Route exact path="/" component={AuthorQuiz} />
+                <Route path="/add" component={AddAuthorForm} />
+            </React.Fragment>
+        </ReactRedux.Provider>
+    </BrowserRouter>, document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
